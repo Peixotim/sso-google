b478c821488bcf8e4984da749bef66ad559fda95
Peixotim <pedropeixotovz@gmail.com>
2026-01-22 15:03:20 -0300

    feat: Adding first migration , what is create-users

---
+++ src/migrations/20260122174837-create-users.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      uuid: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("gen_random_uuid()"),
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      google_id: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      avatar_url: {
+        type: Sequelize.STRING,
+      },
+      role: {
+        type: Sequelize.ENUM("admin", "user"),
+        defaultValue: "user",
+      },      
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_users_role";'
+      );
+  }
+};
