25671a2adf4eba7118cae43f9637382032939041
Peixotim <pedropeixotovz@gmail.com>
2026-01-22 15:10:01 -0300

    feat: Adding method findAll"

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,1 +1,11 @@
-export class UserService{}
+import User from "../entities/users.model.js";
+import { UserRepository } from "../repositories/users.repository.js";
+
+export class UserService{
+  private userRepository = new UserRepository();
+
+
+  public async findAll():Promise<User[]>{
+    return this.userRepository.findAll();
+  }
+}
