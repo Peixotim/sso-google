a6ce61826fccfc07363ef739d5278ef47e293982
Peixotim <pedropeixotovz@gmail.com>
2026-01-23 12:16:55 -0300

    feat: Adicionar método create no UsersController com validação

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,3 +1,4 @@
+import { UserCreateSchema } from "../dtos/user-create-request.dto.js";
 import { UserService } from "../services/users.service.js";
 import { Request, Response } from "express";
 
@@ -10,6 +11,31 @@ export class UsersController {
       return res.status(200).json(users);
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  public async create(req: Request, res: Response) {
+    const validation = UserCreateSchema.safeParse(req.body);
+    if (!validation.success) {
+      return res.status(400).json({
+        message: "Error of Validation",
+        errors: validation.error.format(),
+      });
+    }
+
+    try {
+      const newUser = await this.usersService.create(validation.data);
+      return res.status(201).json(newUser);
+    } catch (error: any) {
+      if (
+        error.message ===
+        "A user with this email address is already registered!"
+      ) {
+        return res.status(409).json({ error: error.message });
+      }
+
+      console.error(error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
 }
