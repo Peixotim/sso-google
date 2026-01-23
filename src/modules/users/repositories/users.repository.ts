78d53ed224672ed6226ebaeb823076a12b352b1a
Peixotim <pedropeixotovz@gmail.com>
2026-01-23 12:17:08 -0300

    feat: Adicionar métodos para busca e verificação de usuário por email

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import { UserCreateDTO } from "../dtos/user-create.dto.js";
 import User from "../entities/users.model.js";
 import { UserRole } from "../enums/UserRole.enum.js";
@@ -7,6 +8,21 @@ export class UserRepository {
     return await User.findAll();
   }
 
+  public async findByEmail(email: string) {
+    const user = await User.findOne({
+      where: { email: email },
+    });
+
+    return user;
+  }
+
+  public async existsUserByEmail(email: string): Promise<boolean> {
+    const count = await User.count({
+      where: { email: email },
+    });
+
+    return count > 0;
+  }
   public async create(data: UserCreateDTO): Promise<User> {
     const newUser = await User.create({
       name: data.name,
