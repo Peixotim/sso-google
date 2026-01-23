9e226f406c1f4a4e3c209b2bd83273fe72b1503f
Peixotim <pedropeixotovz@gmail.com>
2026-01-23 12:17:11 -0300

    feat: Adicionar método create no UserService com validação de usuário

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,10 +1,26 @@
 import User from "../entities/users.model.js";
 import { UserRepository } from "../repositories/users.repository.js";
+import { UserCreateDTO } from "../dtos/user-create.dto.js";
 
 export class UserService {
   private userRepository = new UserRepository();
 
   public async findAll(): Promise<User[]> {
     return this.userRepository.findAll();
+  }
+
+  public async create(data: UserCreateDTO) {
+    const existsUser = await this.userRepository.existsUserByEmail(data.email);
+    if (!data) {
+      throw new Error(`Data is empty !`);
+    }
+
+    if (existsUser) {
+      throw new Error(`A user with this email address is already registered!`);
+    }
+
+    const newUser = await this.userRepository.create(data);
+
+    return newUser;
   }
 }
