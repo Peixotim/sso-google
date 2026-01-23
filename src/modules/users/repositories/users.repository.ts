afd53c44f575926e7b9cad0cc47bf4cc7cbea71f
Peixotim <pedropeixotovz@gmail.com>
2026-01-23 11:34:36 -0300

    feat:Adding method create in Repository

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,13 +1,22 @@
+import { UserCreateDTO } from "../dtos/user-create.dto.js";
 import User from "../entities/users.model.js";
+import { UserRole } from "../enums/UserRole.enum.js";
 
-export class UserRepository{
-  
-  public async findAll():Promise<User[]>{
-    const users = await User.findAll();
-    if(!users){
-      return [];
-    } 
-    
-      return users;
+export class UserRepository {
+  public async findAll(): Promise<User[]> {
+    return await User.findAll();
+  }
+
+  public async create(data: UserCreateDTO): Promise<User> {
+    const newUser = await User.create({
+      name: data.name,
+      email: data.email,
+      googleId: data.googleId || null,
+      avatarUrl: data.avatarUrl || null,
+      role: data.role || UserRole.USER,
+      isActive: data.isActive ?? true,
+    });
+
+    return newUser;
   }
 }
