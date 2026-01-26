a61daaac1e22bac0ab6d6c0fde6cb7926da4a6d3
Peixotim <pedropeixotovz@gmail.com>
2026-01-26 14:08:47 -0300

    refactor: ajustar formatação e organização do código no server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
-import Express from 'express';
-import { configDotenv } from 'dotenv';
-import sequelize from './config/database.js';
+import Express from "express";
 
+import { configDotenv } from "dotenv";
+import sequelize from "./config/database.js";
+import cookieParser from "cookie-parser";
 configDotenv();
 
 const port = Number(process.env.PORT) || 3000;
 const server = Express();
 server.use(Express.json());
+server.use(cookieParser());
 
 server.listen(port, () => {});
 
-async function bootStrap(){
-  try{
+async function bootStrap() {
+  try {
     await sequelize.authenticate();
-    console.log(`Server is running in port ${port}`)
-  }catch(error){
-    console.error(`Error with running the application`)
+    console.log(`Server is running in port ${port}`);
+  } catch (error) {
+    console.error(`Error with running the application`);
     process.exit(1);
   }
 }
