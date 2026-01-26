9a9e2704b3c63a7d9797c7fec58f546f0d728302
Peixotim <pedropeixotovz@gmail.com>
2026-01-26 14:10:09 -0300

    feat: adicionar métodos findByEmail e findOrCreateByGoogle na classe UserService

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,12 +1,24 @@
 import User from "../entities/users.model.js";
 import { UserRepository } from "../repositories/users.repository.js";
 import { UserCreateDTO } from "../dtos/user-create.dto.js";
+import { Profile } from "passport-google-oauth20";
+import { UserRole } from "../enums/UserRole.enum.js";
 
 export class UserService {
   private userRepository = new UserRepository();
 
   public async findAll(): Promise<User[]> {
     return this.userRepository.findAll();
+  }
+
+  public async findByEmail(email: string) {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      return [];
+    }
+
+    return user;
   }
 
   public async create(data: UserCreateDTO) {
@@ -20,6 +32,50 @@ export class UserService {
     }
 
     const newUser = await this.userRepository.create(data);
+
+    return newUser;
+  }
+
+  public async findOrCreateByGoogle(profile: Profile) {
+    const email = profile.emails?.[0]?.value;
+    const google_id = profile.id;
+    const avatarUrl = profile.photos?.[0]?.value || "";
+    const name = profile.displayName;
+
+    if (!email) {
+      throw new Error(`Google account without email`);
+    }
+
+    let user = await User.findOne({
+      where: { google_id: google_id },
+    });
+
+    if (user) {
+      //Se já possuir um usuario cadastrado só retornar os dados dele do dba
+      return user;
+    }
+
+    user = await User.findOne({
+      where: { email: email },
+    });
+
+    if (user) {
+      user.google_id = google_id;
+      user.avatarUrl = avatarUrl || user.avatarUrl;
+
+      await user.save();
+
+      return user;
+    }
+
+    const newUser = await User.create({
+      email: email,
+      google_id: google_id,
+      name: name,
+      avatarUrl: avatarUrl,
+      role: UserRole.USER,
+      isActive: true,
+    });
 
     return newUser;
   }
